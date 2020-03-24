f286d7f9b7d2737cb13b3b10c28e73820f22c224
whl <wuhuanling@xinhuamm.net>
2020-03-24 17:49:35 +0800

    update:视频功能

--- learnsys-admin/src/main/java/com/whl/learnsys/cms/controller/VideoInfoController.java
+++ learnsys-admin/src/main/java/com/whl/learnsys/cms/controller/VideoInfoController.java
@@ -1,14 +1,17 @@
 package com.whl.learnsys.cms.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.whl.common.enums.ResultCode;
+import com.whl.common.models.ResultModel;
 import com.whl.common.models.VideoInfoEntity;
+import com.whl.common.param.PageParam;
 import com.whl.common.service.VideoInfoService;
 import com.whl.common.util.R;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 
@@ -28,6 +31,21 @@ public class VideoInfoController {
      * 列表
      */
 
+    @ApiOperation(value = "获取用户列表", notes = "获取用户列表")
+    @ApiImplicitParam(name = "pageParam", value = "获取参数", required = true, dataType = "PageParam")
+    @PostMapping( "/list")
+    public ResultModel<Page<VideoInfoEntity>> list(@RequestBody PageParam pageParam) {
+
+        Page<VideoInfoEntity> page = new Page<>(pageParam.getCurrent(), pageParam.getSize());
+        Page<VideoInfoEntity> page1 = videoInfoService.page(page);
+        int hasMore = 0;
+        if (page1.getPages() >= pageParam.getCurrent()) {
+            hasMore = 1;
+        }
+
+
+        return ResultModel.valueOf(ResultCode.FAILURE,page1,null,hasMore);
+    }
 
     /**
      * 信息
